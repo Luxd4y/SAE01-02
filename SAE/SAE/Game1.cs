0312023211d75a891909c30f2a2a5af42fb22c51
Luxd4y <hidri.imene@etu.univ_smb.fr>
2023-01-03 16:33:03 +0100

    Player initialisation

--- SAE/SAE/Game1.cs
+++ SAE/SAE/Game1.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
+using MonoGame.Extended.Sprites;
 
 namespace SAE
 {
@@ -9,15 +10,27 @@ namespace SAE
         private GraphicsDeviceManager _graphics;
         private SpriteBatch _spriteBatch;
 
+        //Joueur
+        private Vector2 _positionPlayer;
+        private AnimatedSprite _player;
+        Player playerbase = new Player();
+
+        //
+
         public Game1()
         {
             _graphics = new GraphicsDeviceManager(this);
             Content.RootDirectory = "Content";
             IsMouseVisible = true;
         }
-
+       
         protected override void Initialize()
         {
+            //Joueur
+
+            _positionPlayer = new Vector2(0, 0);
+            //
+
             // TODO: Add your initialization logic here
 
             base.Initialize();
@@ -35,9 +48,23 @@ namespace SAE
             if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
                 Exit();
 
-            // TODO: Add your update logic here
+            KeyboardState keyboardState = Keyboard.GetState();
+            float deltaSeconds = (float)gameTime.ElapsedGameTime.TotalSeconds; // DeltaTime
+            float walkSpeed = deltaSeconds; // Vitesse de déplacement du sprite
 
-            base.Update(gameTime);
+            //player bouge
+            if (keyboardState.IsKeyDown(Keys.Up))
+                _positionPlayer.Y += 1;
+            else if (keyboardState.IsKeyDown(Keys.Down))
+                _positionPlayer.Y -= 1;
+            else if (keyboardState.IsKeyDown(Keys.Left))
+                _positionPlayer.X -= 1;
+            else if (Keyboard.GetState().IsKeyDown(Keys.Right))
+                _positionPlayer.X += 1;
+
+
+                // TODO: Add your update logic here
+                base.Update(gameTime);
         }
 
         protected override void Draw(GameTime gameTime)
--- SAE/SAE/Player.cs
+++ SAE/SAE/Player.cs
@@ -3,10 +3,108 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.Xna.Framework.Graphics;
+using MonoGame.Extended.Content;
+using MonoGame.Extended.Serialization;
+using MonoGame.Extended.Sprites;
+using MonoGame.Extended.Tiled;
+using MonoGame.Extended.Tiled.Renderers;
 
 namespace SAE
 {
     internal class Player
     {
+        //private sprite 
+        private SpriteBatch spriteBatch;
+        private string pseudo;
+        private int points_de_vie;
+        private int vitesse;
+        private int coins;
+
+        public Player(string pseudo, int points_de_vie, int vitesse, int coins)
+        {
+            this.Pseudo = pseudo;
+            this.Points_de_vie = points_de_vie;
+            this.Vitesse = vitesse;
+            this.Coins = coins;
+        }
+
+        public string Pseudo
+        {
+            get
+            {
+                return this.pseudo;
+            }
+
+            set
+            {
+                this.pseudo = value;
+            }
+        }
+
+        public int Points_de_vie
+        {
+            get
+            {
+                return this.points_de_vie;
+            }
+
+            set
+            {
+               
+                this.points_de_vie = value;
+            }
+        }
+
+        public int Vitesse
+        {
+            get
+            {
+                return this.vitesse;
+            }
+
+            set
+            {
+                this.vitesse = value;
+            }
+        }
+
+        public int Coins
+        {
+            get
+            {
+                return this.coins;
+            }
+
+            set
+            {
+                if (value != 0)
+                    throw new ArgumentException("Le Joueur ne peut pas avoir des coins au debut de la partie");
+                this.coins = value;
+            }
+        }
+
+        public override bool Equals(object obj)
+        {
+            return obj is Player player &&
+                   this.Pseudo == player.Pseudo &&
+                   this.Points_de_vie == player.Points_de_vie &&
+                   this.Vitesse == player.Vitesse &&
+                   this.Coins == player.Coins;
+        }
+
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(this.Pseudo, this.Points_de_vie, this.Vitesse, this.Coins);
+        }
+
+        public override string ToString()
+        {
+            return base.ToString();
+        }
+
+
+         
+
     }
 }
